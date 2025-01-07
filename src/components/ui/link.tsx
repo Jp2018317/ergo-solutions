a78b828094050890df7da3dc501a1485c816cc37
jmorales-2018317 <jmorales-2018317@kinal.edu.gt>
2025-01-07 17:39:14 -0600

    fix: link props type

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,19 +1,18 @@
- 
-import { default as LinkNext, type LinkProps } from 'next/link';
-import { VariantProps } from 'class-variance-authority';
+import {default as LinkNext, type LinkProps} from 'next/link';
+import {VariantProps} from 'class-variance-authority';
 import React from 'react';
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@components/button';
+import {cn} from '@/lib/utils';
+import {buttonVariants} from '@components/button';
 
 export interface LinkYubiProps
-  extends LinkProps, VariantProps<typeof buttonVariants> {
+    extends LinkProps, VariantProps<typeof buttonVariants> {
   className?: string
   children: React.ReactNode,
-  color?: string
+  color?: string,
 }
 
 const Link = React.forwardRef<HTMLAnchorElement, LinkYubiProps>(({
-  variant = 'subtleLink', size = 'link', className, ...props
+  variant = 'link', size = 'default', className, ...props
 }, ref) => (
     <LinkNext
       className={cn(buttonVariants({ variant, size, className }))}
