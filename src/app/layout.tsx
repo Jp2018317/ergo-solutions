429c25cd748c9b926fa0d011ea08b64781f4e093
jmorales-2018317 <jmorales-2018317@kinal.edu.gt>
2025-01-07 17:13:13 -0600

    feat: add navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import outfit from '@/app/fonts';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
+import {config} from '@fortawesome/fontawesome-svg-core';
+import InstancedNav from "@components/InstancedNav";
+
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
@@ -21,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${outfit.variable} bg-white`}
       >
+        <InstancedNav />
         {children}
       </body>
     </html>
