0c93aa82893cb90adfa7f7b26c2eee6a52c79abc
jmorales-2018317 <jmorales-2018317@kinal.edu.gt>
2025-02-20 19:57:45 -0600

    fix: button design

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,34 +5,53 @@ import {cva, type VariantProps} from "class-variance-authority"
 import {cn} from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-primary-300 text-primary-foreground shadow hover:bg-primary-300/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-white shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary-300 text-secondary-foreground shadow-sm hover:bg-secondary-300/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
-      },
+    `inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm
+  font-medium transition-colors focus-visible:outline-none focus-visible:ring-2
+  focus-visible:ring-action-light-green focus-visible:ring-offset-0 disabled:text-gray-300
+  disabled:pointer-events-none text-action`,
+    {
+        variants: {
+            variant: {
+                primary:
+                    `bg-primary-300 transition-all bg-[position:_0%_0%] hover:bg-[position:_100%_100%] bg-[size:_200%] 
+                     bg-gradient-to-r from-primary-300 to-primary-200  text-white py-[14px] px-[24px] hover:bg-gradient-to-r 
+                     hover:from-primary-300 hover:to-primary-200 active:bg-gradient-to-r active:from-primary-200 active:to-primary-100
+                     disabled:bg-none disabled:text-gray-400 disabled:bg-gray-100`,
+                destructive:
+                    `bg-auxiliary-brick text-white hover:bg-auxiliary-brick/90 disabled:text-gray-400
+                     disabled:bg-gray-100`,
+                outline:
+                    `border border-black bg-background hover:bg-gray-100 disabled:bg-gray-100
+                     disabled:text-gray-400 disabled:bg-gray-500/40`,
+                secondary:
+                    `transition-all bg-secondary-500 text-white hover:bg-secondary-400
+                     active:bg-action-400/80 text-black disabled:text-gray-400 text-white`,
+                ghost:
+                    `shadow-none hover:bg-gray-100 hover:text-accent-foreground 
+                    disabled:text-gray-300 disabled:bg-black`,
+                subtleLink:
+                    'focus-visible:ring-black shadow-none text-black underline-offset-4 hover:underline',
+                underlineLink:
+                    'focus-visible:ring-black shadow-none text-black underline-offset-4 underline',
+                legalLink:
+                    'focus-visible:ring-black shadow-none text-primary-dark-500 focus-visible:ring-black',
+                iconLink:
+                    'focus-visible:ring-black shadow-none focus-visible:ring-0 hover:cursor-pointer',
+            },
+            size: {
+                default: 'h-[42px] rounded-lg px-[18px] py-3',
+                sm: 'h-[34px] rounded-md px-[12px] py-[18px]',
+                lg: 'h-14 rounded-lg px-[24px] py-[14px]',
+                icon: 'h-10 w-10',
+                link: 'h-fit w-fit px-1 py-[1px]',
+            },
+        },
+        defaultVariants: {
+            variant: 'primary',
+            size: 'default',
+        },
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -4,22 +4,23 @@ import React from 'react';
 import {cn} from '@/lib/utils';
 import {buttonVariants} from '@components/button';
 
-export interface LinkYubiProps
+export interface LinkCustomProps
     extends LinkProps, VariantProps<typeof buttonVariants> {
   className?: string
   children: React.ReactNode,
-  color?: string,
 }
 
-const Link = React.forwardRef<HTMLAnchorElement, LinkYubiProps>(({
-  variant = 'link', size = 'default', className, ...props
-}, ref) => (
-    <LinkNext
-      className={cn(buttonVariants({ variant, size, className }))}
-      ref={ref}
-      {...props}
-    />
-  ));
+const Link = React.forwardRef<HTMLAnchorElement, LinkCustomProps>(({
+  variant = 'legalLink', size = 'link', className, ...props
+}, ref) => {
+  return (
+      <LinkNext
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+      />
+  );
+});
 
 Link.displayName = 'Link';
 
