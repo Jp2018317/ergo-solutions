7dd5efc392ae6d2313463f50701885ecb3efa297
jmorales-2018317 <jmorales-2018317@kinal.edu.gt>
2025-02-23 19:21:35 -0600

    fix: delete unused socials from footer

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import DetecomWhite from '@public/logo/DetecomWhite.webp';
+import {faWhatsapp} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faContactCard} from "@fortawesome/free-solid-svg-icons";
+import {WHATSAPP_NUMBER} from "@/config";
 
 
 export default async function FooterComponent() {
@@ -13,24 +17,23 @@ export default async function FooterComponent() {
           >
             <Image
                 alt="logo"
-                className="w-full max-w-60  h-full object-contain block dark:hidden sm:ml-10"
+                className="w-full max-w-60 h-full object-contain block dark:hidden sm:ml-10"
                 src={DetecomWhite}
                 fill
             />
           </Link>
         </section>
-        <section className='w-full flex justify-around max-md:pt-10 max-w-[1150px] max-xl:max-w-[750px] max-lg:max-w-[600px]'>
-          <div className='flex flex-col justify-center text-white text-center gap-4'>
-            <h6 className='font-bold mb-2 max-sm:text-action'>Redes Sociales:</h6>
-            <span>Facebook</span>
-            <span>Instagram</span>
-            <span>TikTok</span>
-            <span>YouTube</span>
-          </div>
-          <div className='flex flex-col justify-center text-white text-center gap-4'>
+        <section className='w-full flex justify-center md:justify-end max-md:pt-10 max-w-[1150px] max-xl:max-w-[750px] max-lg:max-w-[600px]'>
+          <div className='flex flex-col justify-center items-center text-white text-center gap-4 sm:mr-10'>
             <h6 className='font-bold mb-2 max-sm:text-action'>Contacto:</h6>
-            <span>Email</span>
-            <span>WhatsApp</span>
+            <div className='flex gap-2'>
+                <FontAwesomeIcon icon={faContactCard} />
+            <   span>Email</span>
+            </div>
+              <a className='flex gap-2' href={`https://api.whatsapp.com/send?phone=${WHATSAPP_NUMBER}`} target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faWhatsapp} />
+                <span>WhatsApp</span>
+            </a>
           </div>
         </section>
       </footer>
